da511a14607266ef71fc4e0b16bf1a28dd7f15cb
JoshuaMelo <joshuamelo1126@gmail.com>
2018-08-31 11:09:36 -0400

    Create GetWindowsUpdatesHistory.ps1

---
+++ GetWindowsUpdatesHistory.ps1
@@ -0,0 +1,19 @@
+$scriptLoc = (Get-Location)
+write-host "This script gets Windows Updates from workstations"
+
+$computersArray = get-content -path  $scriptLoc\Computers.txt
+write-host "Working, please wait..."
+	foreach ($computers in $computersArray){
+
+		if(test-connection -computerName $computers -Quiet -count 1)
+		{
+		write-host $computers "is online. writing to WindowsUpdateReview.txt..."
+		get-hotfix -computerName $computers | select  pscomputername, hotfixid, installedon | sort installedon  | export-csv $scriptLoc\WindowsUpdates.csv -append -notypeinformation
+		}
+		
+		else{
+		write-host $computers "is not online"
+		}
+}
+
+read-host "Results located at $scriptLoc"
