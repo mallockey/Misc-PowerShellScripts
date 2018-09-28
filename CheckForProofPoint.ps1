ee8369c033b1814d70ba728763a58ace9aa4195e
JoshuaMelo <joshuamelo1126@gmail.com>
2018-09-28 17:52:00 -0400

    Update CheckForProofPoint.ps1

--- CheckForProofPoint.ps1
+++ CheckForProofPoint.ps1
@@ -1,5 +1,24 @@
-$scriptLoc = (Get-Location)
-$computersArray = get-content -path $scriptLoc\computers.txt
+try{
+import-module activedirectory -erroraction stop
+}
+catch{
+write-host -foreGroundColor red "Run this from a DC"
+read-host
+exit
+}
+
+$ou = read-host "Enter the OU where the PCs are"
+
+try
+{
+    $computersArray = get-adcomputer -filter * -searchbase $ou| select -expandproperty name
+}
+catch
+{
+    write-host -foreGroundColor red "OU not correct please verify OU and rerun."
+    read-host 
+    exit
+}
 
 	foreach($computer in $computersArray){
 	$PP32 = "\\$computer\c$\Program Files (x86)\Proofpoint"
@@ -8,10 +27,10 @@ $computersArray = get-content -path $scriptLoc\computers.txt
 	if(test-connection $computer -count 1 -quiet){
 	
 			if(test-path -path $PP32){
-			write-host -foreGroundColor green $computer "has Proofpoint 32 installed."
+			write-host -foreGroundColor green $computer "has Proofpoint 32 bit installed."
 			}
 			elseif(test-path -path $PP64){
-			write-host -foreGroundColor green $computer "has Proofpoint 64 installed."
+			write-host -foreGroundColor green $computer "has Proofpoint 64 bit installed."
 			}
 			else{
 			write-host "$computer - not installed"
