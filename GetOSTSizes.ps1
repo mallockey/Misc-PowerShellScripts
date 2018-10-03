382460ec4dbc060eae02ff52ae859312d699bdf2
JoshuaMelo <joshuamelo1126@gmail.com>
2018-10-03 17:32:29 -0400

    Update GetOSTSizes.ps1

--- GetOSTSizes.ps1
+++ GetOSTSizes.ps1
@@ -1,26 +1,35 @@
 $computersArray = get-content computers.txt
-foreach($computer in $computersArray){
- 
-    $ostArray = Get-ChildItem -Path "\\$computer\c$\users\*\appdata\local\microsoft\outlook\*.ost
-    write-host "============================================================"
-    write-host $computer
-    write-host "============================================================"
-        foreach($ost in $ostArray){
-        $ostLength = $ost.length
-        $ostLengthInGB = $ostLength / 1gb
-        $ostLengthInGb = [int]$ostLengthInGb
-        $ostTotalSum += $ost.length
-        $ostTotalSumInGb = $ostTotalSum / 1gb
-        $ostTotalSumInGb = [int]$ostTotalSumInGb
 
-        write-host $ost.name "|" $ostLengthInGB
-        }
-        $ostTotalSum = $ostTotalSum / 1gb
-        $ostTotalSum = [int]$ostTotalSum
-        write-host Total Sum: $ostTotalSum "GBs"
-        
-write-host "============================================================"
-write-host "End of "$computer
-write-host "============================================================"
-}
-read-host "wait"
+	foreach($computer in $computersArray){
+
+	$ostArray = Get-ChildItem -Path "\\$computer\c$\users\*\appdata\local\microsoft\outlook\*.ost"
+	
+		write-host $computer
+		$totalOSTsOnPC = 0
+		
+			foreach($ost in $ostArray){
+			
+			$ostLength = $ost.length
+			$ostLengthInGB = $ostLength / 1gb
+			$ostLengthInGb = [int]$ostLengthInGb
+			$ostTotalSum += $ost.length
+			$ostTotalSumInGb = $ostTotalSum / 1gb
+			$ostTotalSumInGb = [int]$ostTotalSumInGb
+			$totalOSTsOnPC++
+			write-host $ost.name "|" $ostLengthInGB"GBs"
+			
+			}
+			$ostTotalSum = $ostTotalSum / 1gb
+			$ostTotalSum = [int]$ostTotalSum
+				if($ostTotalSum -gt 30){
+				write-host -foregroundColor yellow "Total Size all OSTS:$ostTotalSum GBs "Warning, a lot of space is being used""
+				}
+				else{
+				write-host Total Size all OSTS: $ostTotalSum "GBs"
+				}
+				write-host "Total OSTs:"$totalOSTsOnPC
+				
+			write-host "============================================================"
+				
+	}
+	read-host "wait"
