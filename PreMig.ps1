8998eff01d5554ff4f7455e1a1d41b58ff7aa41a
JoshuaMelo <joshuamelo1126@gmail.com>
2018-11-27 12:35:59 -0500

    Update PreMig.ps1

--- PreMig.ps1
+++ PreMig.ps1
@@ -1,30 +1,21 @@
 <#
 Author: Josh Melo
 Last Updated: 11/24/18
-This script is used for backing up user specific data during a mail migration.
-AutoComplete, Signatures, (Mailbox Rules will be coming soon)
-and screenshots of Outlook Mail tab, Outlook Calendar tab, and Outlook Contacts tab
-are automaticlly taken and stored in $mailMigrationFolder. PST files wil be checked for
-under the users folder and the location will be noted.
 #>
+write-output "
+-----------------------------------------------------------------------------
+This script is used for backing up user specific data during a mail migration.
+AutoComplete, Signatures, and screenshots of Outlook Mail tab, 
+Outlook Calendar tab, and Outlook Contacts tab
+are automaticlly taken and stored in C:\Kits\MailMigration. PST files 
+wil be checked for under the users folder and the location will be noted.
+-----------------------------------------------------------------------------"
+
+start-sleep -seconds 5
 
 $currentUserProfile = $env:USERPROFILE
 $mailMigrationFolder = "C:\Kits\MailMigration"
 $currentUserFolder = split-path $currentUserProfile -leaf
-
-function testPath($path)
-{
- $path = test-path $path
-	 if($path -eq $true)
-	 {
-	 return $true
-	 }
-	 else
-	 {
-	 return $false
-	 }
-
-}
 
 function infoText($output)
 {
@@ -41,12 +32,62 @@ function successText($output){
 	$output = write-host -foreGroundColor green "SUCCESS:"$output
 	return $output
 }
+
+function testPath($path)
+{
+ $path = test-path $path
+	 if($path -eq $true)
+	 {
+	 return $true
+	 }
+	 else
+	 {
+	 return $false
+	 }
+
+}
+
+function getRules(){
+#This code was taken from Scripting Guy blog here:
+#https://blogs.technet.microsoft.com/heyscriptingguy/2009/12/15/hey-scripting-guy-how-can-i-tell-which-outlook-rules-i-have-created/
+
+Add-Type -AssemblyName microsoft.office.interop.outlook 
+$olFolders = "Microsoft.Office.Interop.Outlook.OlDefaultFolders" -as [type]
+$outlook = New-Object -ComObject outlook.application
+$namespace  = $Outlook.GetNameSpace("mapi")
+$folder = $namespace.getDefaultFolder($olFolders::olFolderInbox)
+$rules = $outlook.session.DefaultStore.GetRules()
+$rules |
+Sort-Object -Property ExecutionOrder |
+Format-Table -Property Name, ExecutionOrder, Enabled, isLocalRule -AutoSize
+$rules | export-csv "$mailMigrationFolder\Rules.csv"
+foreach($rule in $rules){
+	if($rule.IsLocalRule -eq $true){
+	write-host $rule "is local. Please consult PTM"
+	}
+}
+
+}
 function takeScreenShot{
 
 	Param(
 	$fileName
 	)
+    write-host "Please have Outlook open on screen and maximized."
+    write-host "Close any expanded mailboxes/calendars/contacts to allow maximum view"
+	write-host "---------------------------------------------"
+    $i = 16
+    while($i -ge 0)
+    {
+	$i--
+        if($i -eq 0){
+        write-host "SCREENSHOT TIME"
+        break
+        }
 
+    start-sleep -seconds 1
+    write-host "Screenshot in: $i seconds"
+    }
 	Add-Type -AssemblyName System.Windows.Forms
 	Add-type -AssemblyName System.Drawing
 
@@ -63,8 +104,16 @@ function takeScreenShot{
 	$graphic.CopyFromScreen($Screen.Left, $Screen.Top, 0, 0, $bitmap.Size)
 
 	# Save to file
+    try{
 	$bitmap.Save($fileName)
 	successText( "Screenshot saved to: $fileName")
+	write-host "---------------------------------------------"
+    }
+
+    catch{
+        failText("Taking screenshot, please rerun script.")
+        exit
+    }
 	
 
 }
@@ -90,33 +139,30 @@ function postChecks{
 $testMailMigrationFolder = testPath("C:\Kits\MailMigration")
 if($testMailMigrationFolder -eq $false){
 	infoText("Creating C:\Kits\MailMigration Folder")
-	write-host("-------------------------")
-	new-item -ItemType directory -Path C:\kits\MailMigration
-	write-host("-------------------------")
+	#Only in variable to silently make the directory
+	$mailDirectory = new-item -ItemType directory -Path C:\kits\MailMigration
+	
 }
 else{
-write-host "C:\Kits\MailMigration already exists, saving files there"
+infoText("C:\Kits\MailMigration already exists, saving files there")
 }
 
-#
-
+start-sleep -seconds 5
 #Screenshot Outlook Mail View
 start-process outlook.exe 
-start-sleep -seconds 10
 $fileName = "$mailMigrationFolder\OutlookView.bmp"
 takeScreenShot -fileName $fileName
 
 #Screenshot Outlook Calendar View
 start-process outlook.exe -argumentlist "/select outlook:calendar"
-start-sleep -seconds 10
 $fileName = "$mailMigrationFolder\OutlookCalendarView.bmp"
 takeScreenShot -fileName $fileName
 
 #Screenshot Outlook ContactsView
 start-process outlook.exe -argumentlist "/select outlook:contacts"
-start-sleep -seconds 10
 $fileName = "$mailMigrationFolder\OutlookContactsView.bmp"
 takeScreenShot -fileName $fileName
+
 $currentUserProfile = $env:USERPROFILE
 $autoComplete = $currentUserProfile+"\appdata\local\microsoft\outlook\roamcache\"
 $signatures = $currentUserProfile+"\appdata\roaming\microsoft\signatures"
@@ -125,20 +171,21 @@ $autoCompleteTest = testPath($autoComplete)
 if($autoCompleteTest -eq $true){
 	successText ("AutoComplete found, backing up to C:\Kits\MailMigration")
 	copy-item -Path $autoComplete -destination "$mailMigrationFolder" -recurse
+	write-host "---------------------------------------------"
 }
 else{
 	infoText("No RoamCache folder found under $autoComplete")
-
 }
 
 $autoCompleteTest = testPath($signatures)
 if($autoCompleteTest -eq $true){
 	successText ("Signatures found, backing up to C:\Kits\MailMigration")
 	copy-item -Path $signatures -destination "$mailMigrationFolder" -recurse
+	write-host "---------------------------------------------"
 }
+
 else{
 	infoText("No Signatures folder found under $signatures")
-
 }
 write-host "---------------------------------------------"
 write-host "Checking for PSTS under $currentUserProfile"
@@ -150,27 +197,32 @@ $index = 1
 	else{
 		write-host "---------------------------------------------"
 		write-host "Found some!"
-		foreach ($pst in $psts){
-		write-host "$index)"$PST.name 
-		write-host $PST.directory
-		$index++
+            foreach ($pst in $psts){
+            write-host "$index)"$PST.name 
+            write-host $PST.directory
+            $index++
 		}
 	}
+
+write-host "--------------------------Checking for Rules------------------------------"
+start-process outlook.exe
+getRules
+
 write-host "--------------------------------Results-----------------------------------------"
 $postCheckAutoComplete = "$mailMigrationFolder\roamcache\"
 $postCheckAutoComplete = postChecks -test $postCheckAutoComplete
 write-host "1)"$postCheckAutoComplete
 
-
 $postCheckSignatures = "$mailMigrationFolder\Signatures\"
 $postCheckSignatures = postChecks -test $postCheckSignatures
 write-host "2)"$postCheckSignatures
 
-if($PSTS -ne $null){
-write-host "3) PSTS were found please check above folders"
-}
+    if($PSTS -ne $null){
+    write-host "3) PSTS were found please check above folders"
+    }
 
 write-host "----------------------------End Of Results--------------------------------------"
-write-host "Press Enter to exit"
-read-host
 
+while($confirmation -ne "*"){
+$confirmation = read-host "Enter * to exit"
+}
