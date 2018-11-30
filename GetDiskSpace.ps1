375b953060bd551a20aaef80b60cc186c34de2b2
JoshuaMelo <joshuamelo1126@gmail.com>
2018-11-29 23:15:46 -0500

    Update GetDiskSpace.ps1

--- GetDiskSpace.ps1
+++ GetDiskSpace.ps1
@@ -12,8 +12,8 @@ function getFreeSpace{
 	)
 	try 
 	{	
-		$freeSpace = Get-wmiobject -class win32_logicaldisk -computername $server  | Where-Object { $_.DeviceID -eq "$driveLetter"} | select-object -expandproperty freespace -erroraction stop
-		$freeSpace = $freeSpace / 1gb
+	$freeSpace = Get-wmiobject -class win32_logicaldisk -computername $server  | Where-Object { $_.DeviceID -eq "$driveLetter"} | select-object -expandproperty freespace -erroraction stop
+	$freeSpace = $freeSpace / 1gb
 	
 	}
 	catch{
@@ -30,26 +30,29 @@ function getTotalSpace{
 	)
 
 	try {	
-		$totalSpace = Get-wmiobject -class win32_logicaldisk -computername $server  | Where-Object { $_.DeviceID -eq "$driveLetter"} | select-object -expandproperty size -erroraction stop
-		$totalSpace = $totalSpace / 1gb
-		}
+	     $totalSpace = Get-wmiobject -class win32_logicaldisk -computername $server  | Where-Object { $_.DeviceID -eq "$driveLetter"} | select-object -expandproperty size -erroraction stop
+	     $totalSpace = $totalSpace / 1gb
+	    }
 		
 	catch{
-		$totalSpace = 0
-	}
+	     $totalSpace = 0
+	     }
 	return $totalSpace
 }
 foreach($server in $servers){
 
-	foreach($driveLetter in $driveLetters){	
+	foreach($driveLetter in $driveLetters)
+	{	
 		[int]$freeSpace = getFreeSpace -driveLetter $driveLetter -server $server	
 		[int]$totalSpace = getTotalSpace -driveLetter $driveLetter -server $server
-			if($freeSpace -eq 0 -or $totalSpace -eq 0){
-			write-output "$server does not have an $driveLetter drive" | out-file $psscriptroot\$todaysDate.txt -append
-			}
-			else{
-			write-output "$server $driveLetter $freeSpace GBs free of $totalSpace GBs" | out-file $psscriptroot\$todaysDate.txt -append
-			}
+		  if($freeSpace -eq 0 -or $totalSpace -eq 0)
+		  {
+	           write-output "$server does not have an $driveLetter drive" | out-file $psscriptroot\$todaysDate.txt -append
+		  }
+		  else
+		  {
+		  write-output "$server $driveLetter $freeSpace GBs free of $totalSpace GBs" | out-file $psscriptroot\$todaysDate.txt -append
+		  }
 
 	}
 		write-output "--------------------------------------------------------" | out-file $psscriptroot\$todaysDate.txt -append
