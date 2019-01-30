f8a7180e546f89d27bf9787b51d820b0a36434e8
Joshua Melo <joshuamelo1126@gmail.com>
2019-01-30 09:34:06 -0500

    Update CreateMultipleFolders.ps1

--- CreateMultipleFolders.ps1
+++ CreateMultipleFolders.ps1
@@ -5,21 +5,16 @@ Param(
   $childPath
 )
 
-$childPath = $childPath.Split(" ")
-
+$childPath = $childPath.Split(",")
 $testIfDirectoryExists = test-path -path $directory
-	if($testIfDirectoryExists -eq $false){
-	$makeNewYearDir = New-Item -Type directory "$directory\$parentPath"
-	}
-	foreach($children in $childPath){
-		$testIfChildExists = test-path -path "$directory\$parentPath\$children"
-		if($testIfChildExists-eq $false){
-		$makeNewChildDir =  New-Item -Type directory "$directory\$parentPath\$children"
-		Write-Progress -Activity "Creating Folders" -CurrentOperation "Current Folder: $children"
-		start-sleep -seconds .65
-		}
-	}
-
-
-
-#Do stuff below with $finalDirectory
+    if($testIfDirectoryExists -eq $false){
+    $makeNewYearDir = New-Item -Type directory "$directory\$parentPath"
+    }
+        foreach($children in $childPath){
+	$testIfChildExists = test-path -path "$directory\$parentPath\$children"
+	    if($testIfChildExists-eq $false){
+	    $makeNewChildDir =  New-Item -Type directory "$directory\$parentPath\$children"
+	    Write-Progress -Activity "Creating Folders" -CurrentOperation "Current Folder: $children"
+	    start-sleep -seconds .65
+	    }
+	 }
