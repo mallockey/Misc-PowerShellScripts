02da56446f77b66acfadd68ecbf260d9a656b541
Joshua Melo <joshuamelo1126@gmail.com>
2019-01-31 00:33:47 -0500

    Update CreateFolderFromMonth.ps1

--- CreateFolderFromMonth.ps1
+++ CreateFolderFromMonth.ps1
@@ -1,6 +1,5 @@
 $subDirectory = "C:\Users\Jmelo\desktop\test"
 $monthsToNumbers = @{
-
     "01" = "January"
     "02" = "February"
     "03" = "March"
@@ -13,39 +12,31 @@ $monthsToNumbers = @{
     "10" = "October"
     "11" = "November"
     "12" = "December"
-
 }
 
 function getCurrentMonthFolder{
-	Param(
-	$date
-	)
-	
+    Param(
+    $date
+    )	
 $todaysMonth = $date.Substring(0,2)
 $month = $monthsToNumbers[$todaysMonth]
 $year = $date.Substring(6)
-
 $testIfYearExists = test-path -path $subDirectory\$year	
-	if($testIfYearExists -eq $false){
-	$makeNewYearDir = New-Item -Type directory "$subDirectory\$year"
-	}
-
+    if($testIfYearExists -eq $false){
+    $makeNewYearDir = New-Item -Type directory "$subDirectory\$year"
+    }
 $testIfMonthExists = test-path -path $subDirectory\$year\$month
-	if($testIfMonthExists -eq $false){
-	$makeNewMonthDir =  New-Item -Type directory "$subDirectory\$year\$month"
-	}
-
-	$finalDirectory = "$subDirectory\$year\$month"
-	return $finalDirectory
+    if($testIfMonthExists -eq $false){
+    $makeNewMonthDir =  New-Item -Type directory "$subDirectory\$year\$month"
+    }
+$finalDirectory = "$subDirectory\$year\$month"
+return $finalDirectory
 }
-
-function getDate{
-    
-    $todaysDate = get-date -UFormat %D
-    $todaysDate = $todaysDate.Replace("/","-")
-    $todaysDate = $todaysDate.Insert(6,"20")
-    return $todaysDate
-
+function getDate{  
+$todaysDate = get-date -UFormat %D
+$todaysDate = $todaysDate.Replace("/","-")
+$todaysDate = $todaysDate.Insert(6,"20")
+return $todaysDate
 }
 $currentDate = getDate
 $finalDirectory = getCurrentMonthFolder $currentDate
