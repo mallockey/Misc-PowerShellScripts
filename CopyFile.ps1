24e6ad6d7b0ce2f1aee5afc38a23ebbb128224cc
Joshua Melo <joshuamelo1126@gmail.com>
2019-01-31 00:35:37 -0500

    Update CopyFile.ps1

--- CopyFile.ps1
+++ CopyFile.ps1
@@ -7,27 +7,24 @@ try{
 $computersArray = get-content computers.txt
 }
 catch{
-	write-host "Computers.txt not found"
+write-host "Computers.txt not found"
 }
 
 $WorkSpaceApp = ""
 $app = split-path -leaf $WorkSpaceApp
 foreach($computers in $computersArray){
-
-	if(test-connection -computerName $computers -count 1 -quiet){
-	$computersAndLink = "\\"+$computers+"\c$\kits"
-	try{
-	copy-item -path $WorkSpaceApp -destination $computersAndLink
-	write-host -foreGroundcolor green "Copied $app to $computersAndLink"
-	}
-	catch{
-	write-host -foregroundColor red $computer "copy did not complete"
-	}
-	
-	}
-	else{
-	write-host -foregroundColor red $computers "is not online."
-	}
+    if(test-connection -computerName $computers -count 1 -quiet){
+    $computersAndLink = "\\"+$computers+"\c$\kits"
+    try{
+    copy-item -path $WorkSpaceApp -destination $computersAndLink
+    write-host -foreGroundcolor green "Copied $app to $computersAndLink"
+    }
+    catch{
+    write-host -foregroundColor red $computer "copy did not complete"
+    }	
+    }
+    else{
+    write-host -foregroundColor red $computers "is not online."
+    }
 }
 
-read-host "Done"
