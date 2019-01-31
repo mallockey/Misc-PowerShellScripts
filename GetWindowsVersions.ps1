5fa79f8a48f913bac83f5c6620151d4fc052fc74
Joshua Melo <joshuamelo1126@gmail.com>
2019-01-30 22:09:45 -0500

    Update GetWindowsVersions.ps1

--- GetWindowsVersions.ps1
+++ GetWindowsVersions.ps1
@@ -2,13 +2,13 @@ $ou = read-host "Enter OU of workstations"
 
 try
 {
-    $computersArray = get-adcomputer -filter * -searchbase $ou| select -expandproperty name
+$computersArray = get-adcomputer -filter * -searchbase $ou| select -expandproperty name
 }
 catch
 {
-    write-host -foreGroundColor red "OU not correct please verify OU and rerun."
-    read-host 
-    exit
+write-host -foreGroundColor red "OU not correct please verify OU and rerun."
+read-host 
+exit
 }
 $win7Count = 0
 $win8Count = 0
@@ -17,34 +17,31 @@ $totalPC = 0
 
 write-host -foregroundColor yellow "Working...please wait"
 foreach ($servers in $computersArray){
-if(test-connection -ComputerName $servers -Count 1 -quiet)
-{
-write-host -foreGroundColor green "$servers found, getting OS info"
-$os = get-wmiobject Win32_OperatingSystem -computerName $servers
-$osType = $os.caption
-
-if($osType -like "*Windows 7*")
-{
-$win7Count++
-$totalPC++
-}
-
-elseif($osType -like "*Windows 8*")
-{
-$win8Count++
-$totalPC++
-}
-elseif($osType -like "*Windows 10*")
-{
-$win10Count++
-$totalPC++
-}
-
-}
-else
-{
-write-host -foreGroundColor yellow "$servers is not online"
-}
+    if(test-connection -ComputerName $servers -Count 1 -quiet)
+    {
+    write-host -foreGroundColor green "$servers found, getting OS info"
+    $os = get-wmiobject Win32_OperatingSystem -computerName $servers
+    $osType = $os.caption
+        if($osType -like "*Windows 7*")
+        {
+        $win7Count++
+        $totalPC++
+        }
+        elseif($osType -like "*Windows 8*")
+        {
+        $win8Count++
+        $totalPC++
+        }
+        elseif($osType -like "*Windows 10*")
+        {
+        $win10Count++
+        $totalPC++
+        }
+    }
+    else
+    {
+    write-host -foreGroundColor yellow "$servers is not online"
+    }
 }
 [int]$percent7 = ($win7Count / $totalPC) * 100
 [int]$percent8 = ($win8Count / $totalPC) * 100
