14a04390739020ae6cbd81dc05345134714c8ec8
Joshua Melo <joshuamelo1126@gmail.com>
2019-02-20 17:23:50 -0500

    Create FindUser.ps1

---
+++ FindUser.ps1
@@ -0,0 +1,33 @@
+Param(
+[Parameter(Mandatory=$true)]
+$PCConvention,
+$findUser
+)
+import-module activedirectory
+$computers = Get-ADComputer -Filter * | Where-Object {$_.Name -like "*$PCConvention*"} | select -ExpandProperty name
+$PCsLoggedInto =""
+foreach($computer in $computers){
+
+	if(test-connection -count 1 -quiet $computer){
+		try{
+		$currentUser = get-wmiobject -class win32_computersystem -computername $computer | select -expandproperty username
+		}
+		catch{
+			write-progress -Activity "Checking PCs" -currentOperation "Failed to get data from $computer"
+		}
+		Write-Progress -Activity "Checking PCs" -CurrentOperation "Current PC: $computer"
+		if($currentUser -like "*$findUser*"){
+			$PCsLoggedInto += "$computer "
+		}
+		else{
+		continue
+		}
+	}
+	else{
+	Write-Progress -Activity "Checking PCs" -CurrentOperation "Current PC: $computer is offline"
+	}
+}
+write-host "$findUser was logged into the below PCs:"
+write-host $PCsLoggedInto
+
+read-host
