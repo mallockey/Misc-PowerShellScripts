14d0444acca8bedb6cfb977eec29afd957a2aaa4
Joshua Melo <joshuamelo1126@gmail.com>
2019-03-05 17:10:13 -0500

    Update ADInfo.ps1

--- ADInfo.ps1
+++ ADInfo.ps1
@@ -4,10 +4,9 @@ $computersArray = get-content computers.txt
 $arrayOfInfo = [System.Collections.ArrayList]@()
 foreach($computer in $computersArray){
   try{
-  $ADInfo = get-adcomputer -identity $computer -properties *
+    $ADInfo = get-adcomputer -identity $computer -properties *
   }
   catch{
-
     $description = "NOT IN AD"
     $lastlogondate = "N/A"
     $isEnabled = "N/A"
@@ -21,18 +20,17 @@ foreach($computer in $computersArray){
     continue
   }
 
-	$description = $ADInfo.Description
-	$lastlogondate = $ADInfo.LastLogonDate
-	$isEnabled = $ADInfo.enabled 
-
-	$tempObj = New-Object -TypeName PSObject  
-	$tempObj | Add-Member -MemberType NoteProperty -Name "PCName" -Value $computer
-	$tempObj | Add-Member -MemberType NoteProperty -Name "Description" -Value $description
-	$tempObj | Add-Member -MemberType NoteProperty -Name "LastLogon" -Value $lastlogondate
-	$tempObj | Add-Member -MemberType NoteProperty -Name "Enabled" -Value $isEnabled
-	$arrayOfInfo.Add($tempObj) | out-null 
-	write-host $computers $description "Last Logon Date:"$lastLogonDate "IsEnabled" $isEnabled
-	
+    $description = $ADInfo.Description
+    $lastlogondate = $ADInfo.LastLogonDate
+    $isEnabled = $ADInfo.enabled 
+    
+    $tempObj = New-Object -TypeName PSObject  
+    $tempObj | Add-Member -MemberType NoteProperty -Name "PCName" -Value $computer
+    $tempObj | Add-Member -MemberType NoteProperty -Name "Description" -Value $description
+    $tempObj | Add-Member -MemberType NoteProperty -Name "LastLogon" -Value $lastlogondate
+    $tempObj | Add-Member -MemberType NoteProperty -Name "Enabled" -Value $isEnabled
+    $arrayOfInfo.Add($tempObj) | out-null 
+    write-progress -Activity "Collecting Data" -Status "Current PC: $computer"	
 }
 
 $arrayOfInfo | format-table
