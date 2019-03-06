eb13967ceae763a23291aa4a9a97c2160a0b8d73
Joshua Melo <joshuamelo1126@gmail.com>
2019-03-06 09:26:27 -0500

    Update GetStalePCs.ps1

--- GetStalePCs.ps1
+++ GetStalePCs.ps1
@@ -3,7 +3,6 @@ $currentDir = "$psscriptroot"
 $computersArray = get-adcomputer -filter * | select -expandproperty name
 $arrayOfInfo = [System.Collections.ArrayList]@()
 foreach($computer in $computersArray){
- 
     $ADInfo = get-adcomputer -identity $computer -properties *
     write-progress -Activity "Collecting Data" -Status "Current PC: $computer"	
 
@@ -17,8 +16,6 @@ foreach($computer in $computersArray){
     $tempObj | Add-Member -MemberType NoteProperty -Name "LastLogon" -Value $lastlogondate
     $tempObj | Add-Member -MemberType NoteProperty -Name "Enabled" -Value $isEnabled
     $arrayOfInfo.Add($tempObj) | out-null 
-    
 }
-
 $arrayOfInfo | format-table
 $arrayOfInfo | export-csv $psscriptroot"\StalePCs.csv" -noTypeInformation
