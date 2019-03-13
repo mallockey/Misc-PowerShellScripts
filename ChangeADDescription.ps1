24f5e70217b1f1dfc6a1e4f5ecf365d529ff3e4e
Joshua Melo <joshuamelo1126@gmail.com>
2019-03-13 18:05:40 -0400

    Update ChangeADDescription.ps1

--- ChangeADDescription.ps1
+++ ChangeADDescription.ps1
@@ -2,21 +2,21 @@ import-module activedirectory
 $ou = "OU=Workstations,OU=New York,DC=CorsairInvestments,DC=com"
 $workstations = get-adcomputer -filter * -searchbase $ou | select -expandproperty name
 foreach($workstation in $workstations){
-	if(test-connection -count 1 $workstation -quiet){
-		try{
-			$currentLoggedOnUser = get-wmiobject -class win32_computersystem -computerName $workstation | select -expandproperty username
+    if(test-connection -count 1 $workstation -quiet){
+        try{
+	    $currentLoggedOnUser = get-wmiobject -class win32_computersystem -computerName $workstation | select -expandproperty username
 		
-			$tempIndex = $currentLoggedOnUser.IndexOf("\") + 1
-			$currentLoggedOnUser = $currentLoggedOnUser.SubString($tempIndex)
-			$fullUserName = get-aduser -identity $currentLoggedOnUser | select -expandproperty name
-			write-host $workstation $fullUserName
-		}
-		catch{
-			$currentLoggedOnUser = "No User Logged On"
-			write-host $currentLoggedOnUser
-			continue
-		}
-		Set-ADComputer -Identity $workstation -Description $fullUserName
-	}
+            $tempIndex = $currentLoggedOnUser.IndexOf("\") + 1
+	    $currentLoggedOnUser = $currentLoggedOnUser.SubString($tempIndex)
+	    $fullUserName = get-aduser -identity $currentLoggedOnUser | select -expandproperty name
+	    write-host $workstation $fullUserName
+	 }
+	 catch{
+	     $currentLoggedOnUser = "No User Logged On"
+	     write-host $currentLoggedOnUser
+	     continue
+	 }
+	Set-ADComputer -Identity $workstation -Description $fullUserName
+    }
 }
-asdf
+
