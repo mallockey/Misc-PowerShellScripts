04d6dfc546c2258c62636bbbd2537c91100c82f8
Joshua Melo <joshuamelo1126@gmail.com>
2019-03-15 21:56:37 -0400

    Update ChangeADDescription.ps1

--- ChangeADDescription.ps1
+++ ChangeADDescription.ps1
@@ -5,14 +5,12 @@ foreach($workstation in $workstations){
     if(test-connection -count 1 $workstation -quiet){
         try{
 	    $currentLoggedOnUser = get-wmiobject -class win32_computersystem -computerName $workstation | select -expandproperty username
-      $tempIndex = $currentLoggedOnUser.IndexOf("\") + 1
+            $tempIndex = $currentLoggedOnUser.IndexOf("\") + 1
 	    $currentLoggedOnUser = $currentLoggedOnUser.SubString($tempIndex)
 	    $fullUserName = get-aduser -identity $currentLoggedOnUser | select -expandproperty name
 	    write-host $workstation $fullUserName
 	 }
 	 catch{
-	     $currentLoggedOnUser = "No User Logged On"
-	     write-host $currentLoggedOnUser
 	     continue
 	 }
 	Set-ADComputer -Identity $workstation -Description $fullUserName
