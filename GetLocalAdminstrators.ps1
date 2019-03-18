fe6463572e706db5787b82cd97af47c706cc7e98
Joshua Melo <joshuamelo1126@gmail.com>
2019-03-18 12:55:32 -0400

    Update GetLocalAdminstrators.ps1

--- GetLocalAdminstrators.ps1
+++ GetLocalAdminstrators.ps1
@@ -1,4 +1,5 @@
-$validLocaldAdmins = Get-LocalGroupMember administrators | select sid | ForEach-Object {
-Get-LocalUser $_.sid | where {$_.Enabled -eq $true } | select name, enabled}
+$validLocalAdmins = Get-LocalGroupMember administrators | where-object {$_.PrincipalSource -ne "ActiveDirectory"} | select sid | ForEach-Object {
+Get-LocalUser $_.sid | where {$_.Enabled -eq $true } | select name, enabled
 }
+
 $validLocalAdmins
