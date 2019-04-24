f40259b6b948aa7a467a7e06163dcbfaa3f9051d
Joshua Melo <joshuamelo1126@gmail.com>
2019-04-23 23:52:29 -0400

    Update GetBitlockerStatus.ps1

--- GetBitlockerStatus.ps1
+++ GetBitlockerStatus.ps1
@@ -2,24 +2,22 @@ $OU = ""
 $PCS = Get-ADComputer -filter * -searchbase $OU -properties *
 $arrayOfInfo = [System.Collections.ArrayList]@()
 
- 
 foreach($PC in $PCS){
-	$tempObj = New-Object -TypeName PSObject
-	$DN = $PC.DistinguishedName
-	$PCName = $PC.Name
-	$Description = $PC.Description
-	
-	$recoveryPassword = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $DN  | select msfve-recoverypassword
-	if($recoveryPassword -ne $null){
-		$recoveryPassword = "Good"
-	}
-	else{
-		$recoveryPassword = "NOT BACKED UP"
-	}
-	$tempObj | Add-Member -MemberType NoteProperty -Name PCName -Value $PCName
-	$tempObj | Add-Member -MemberType NoteProperty -Name Description -Value $Description
-	$tempObj | Add-Member -MemberType NoteProperty -Name BackedUpStatus -Value $recoveryPassword
-	$arrayOfInfo.Add($tempObj) | out-null 
+    $tempObj = New-Object -TypeName PSObject
+    $DN = $PC.DistinguishedName
+    $PCName = $PC.Name
+    $Description = $PC.Description
+    $recoveryPassword = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $DN  | select msfve-recoverypassword
+    if($recoveryPassword -ne $null){
+        $recoveryPassword = "Good"
+    }
+    else{
+	 $recoveryPassword = "NOT BACKED UP"
+    }
+    $tempObj | Add-Member -MemberType NoteProperty -Name PCName -Value $PCName
+    $tempObj | Add-Member -MemberType NoteProperty -Name Description -Value $Description
+    $tempObj | Add-Member -MemberType NoteProperty -Name BackedUpStatus -Value $recoveryPassword
+    $arrayOfInfo.Add($tempObj) | out-null 
 }
 
 $arrayOfInfo | export-csv "C:\Kits\BitlockerStatus.csv" -noTypeInformation -Append
