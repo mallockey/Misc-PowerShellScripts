4f86cb54affba1dd47e8742407299e26b4cf0c4f
Joshua Melo <joshuamelo1126@gmail.com>
2019-04-27 22:20:48 -0400

    Added Birthday Reminder

---
+++ BirthdayReminder.ps1
@@ -0,0 +1,28 @@
+$birthdayCSV = Import-Csv "PathtoCSV"
+$todaysDate = Get-Date
+$fromNumber = "EnterTwilioNumberHere"
+$toNumber = "Your number" 
+$accountSID = 'From Twilio'
+$authToken = 'From Twilio'
+$arrayOfBirthdays = [System.Collections.ArrayList]@()
+[Boolean]$upcomingBirthdays = $false
+
+foreach($birthday in $birthdayCSV){
+  $currentName = $birthday.Name
+  $currentName = $currentName+"'s"
+  $currentBirthday = (Get-Date $birthday.Day)
+  $differenceInDays = New-TimeSpan -Start $todaysDate -End $currentBirthday | select -expandproperty Days
+  if($differenceInDays -lt 0 ){
+    continue
+  }
+  if($differenceInDays -lt 30){
+    $upcomingBirthdays = $true
+    $birthdayString = "$currentName birthday is in $differenceInDays days! `n"
+    $arrayOfBirthdays.Add($birthdayString) | Out-Null
+  }
+}
+if($upcomingBirthdays -eq $false){
+  $birthdayString = "There are no birthdays in the next 30 days"
+  $arrayOfBirthdays.Add($birthdayString) | Out-Null
+}
+  C:\Scripts\Send-TwilioSMS.ps1 -AccountSID $accountSID -AuthToken $authToken -FromNumber $fromNumber -ToNumber $toNumber -Message $arrayOfBirthdays
