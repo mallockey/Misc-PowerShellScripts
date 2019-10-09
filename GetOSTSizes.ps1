23b51a1f93d0ca7a52f556509a8f4b0e9283da94
Joshua Melo <joshuamelo1126@gmail.com>
2019-10-08 21:47:10 -0400

    Update GetOSTSizes.ps1

--- GetOSTSizes.ps1
+++ GetOSTSizes.ps1
@@ -4,23 +4,24 @@ $computersArray = get-content computers.txt
     write-host $computer
     $totalOSTsOnPC = 0
         foreach($ost in $ostArray){	
-	$ostLength = $ost.length
-	$ostLengthInGB = $ostLength / 1gb
-	$ostLengthInGb = [int]$ostLengthInGb
-	$ostTotalSum += $ost.length
-	$ostTotalSumInGb = $ostTotalSum / 1gb
-	$ostTotalSumInGb = [int]$ostTotalSumInGb
-	$totalOSTsOnPC++
-	write-host $ost.name "|" $ostLengthInGB"GBs"
+	    $ostLength = $ost.length
+	    $ostLengthInGB = $ostLength / 1gb
+	    $ostLengthInGb = [int]$ostLengthInGb
+	    $ostTotalSum += $ost.length
+	    $ostTotalSumInGb = $ostTotalSum / 1gb
+	    $ostTotalSumInGb = [int]$ostTotalSumInGb
+	    $totalOSTsOnPC++
+	    write-host $ost.name "|" $ostLengthInGB"GBs"
 	}
-	$ostTotalSum = $ostTotalSum / 1gb
-	$ostTotalSum = [int]$ostTotalSum
-	if($ostTotalSum -gt 30){
-	write-host -foregroundColor yellow "Total Size all OSTS:$ostTotalSum GBs "Warning, a lot of space is being used""
-	}
-	else{
-	write-host Total Size all OSTS: $ostTotalSum "GBs"
-	}
-	write-host "Total OSTs:"$totalOSTsOnPC			
-	write-host "============================================================"			
+	    $ostTotalSum = $ostTotalSum / 1gb
+	    $ostTotalSum = [int]$ostTotalSum
+	    
+	    if($ostTotalSum -gt 30){
+	        write-host -foregroundColor yellow "Total Size all OSTS:$ostTotalSum GBs "Warning, a lot of space is being used""
+	    }
+	    else{
+	        write-host Total Size all OSTS: $ostTotalSum "GBs"
+	    }
+    write-host "Total OSTs:"$totalOSTsOnPC			
+    write-host "============================================================"			
     }
