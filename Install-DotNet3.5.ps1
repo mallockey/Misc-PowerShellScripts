dbeb99d4daaf1154e0c663c690aca49162b7553b
Josh Melo <joshuamelo1126@gmail.com>
2020-10-06 12:30:09 -0400

    Update Install-DotNet3.5.ps1

--- Install-DotNet3.5.ps1
+++ Install-DotNet3.5.ps1
@@ -3,7 +3,6 @@
 #>
 
 $ErrorActionPreference = "Stop"
-
 $DotNetInstall = Start-Process "DISM" -ArgumentList "/Online /Enable-Feature /FeatureName:NetFx3 /All /NoRestart" -Wait -PassThru
 
 if(Get-Childitem -Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP" | Where-Object -FilterScript {$_.name -match "v3.5"}){
